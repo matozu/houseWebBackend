79a00cff58ebedffc1fdb953059291c7e6a9f645
matozu <66270231+matozu@users.noreply.github.com>
2022-04-06 19:42:49 +0200

    .

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use("/api/images", ImagesRoutes);
 app.use(helmet());
 app.use(compression);
 
-const port = process.env.PORT || 5000; // PORT or port?
+const port = process.env.PORT || 5000;
 
 const httpServer = http.createServer(app);
 const io = addSocketIO(httpServer);
