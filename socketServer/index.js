d89698fa2ef7442009413a0803a3237312ada21c
matozu <mato.zunac@gmail.com>
2024-05-04 10:50:10 +0200

    obrisane log funkcije iz secket index.js

--- socketServer/index.js
+++ socketServer/index.js
@@ -57,7 +57,6 @@ const addSocketIO = (httpServer) => {
       if (!connectedUsernames.includes(socket.user._username)) {
         connectedUsernames.push(socket.user._username);
       }
-      console.log("cu->" + connectedUsernames);
 
       const usersWithStatus = users.map((u) => {
         if (connectedUsernames.find((cu) => cu === u.username)) {
@@ -70,22 +69,18 @@ const addSocketIO = (httpServer) => {
       socket.emit("connected", { messages: messages, users: usersWithStatus });
 
       socket.broadcast.emit("newUserConnected", socket.user._username);
-
-      console.log("----conection 4 " + socket.user._username + "-------");
       log();
     } catch (e) {
       console.log(e);
     }
 
     socket.on("disconnect", function () {
-      console.log("--------disconnect 4 " + socket.user._username + "--------");
-      log();
       connectedUsernames.splice(
         connectedUsernames.findIndex((u) => u === socket.user._username),
         1
       );
-      console.log("cu->" + connectedUsernames);
       socket.broadcast.emit("disconnected", socket.user._username);
+      log();
     });
 
     socket.on("postMessage", async (postedMessage) => {
