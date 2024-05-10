593ed9abaf499439afde878399e3b727e7df640c
matozu <mato.zunac@gmail.com>
2024-05-10 12:35:00 +0200

    HEAD detached commit

--- socketServer/index.js
+++ socketServer/index.js
@@ -38,10 +38,10 @@ const addSocketIO = (httpServer) => {
 
   io.use(middleware);
 
-  function log() {
-    console.log("connected users: ");
-    io.sockets.sockets.forEach((s) => console.log(s.user._username));
-  }
+  // function log() {
+  //   console.log("connected users: ");
+  //   io.sockets.sockets.forEach((s) => console.log(s.user._username));
+  // }
 
   io.on("connection", async (socket) => {
     try {
